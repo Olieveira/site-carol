630cac5266a9620c42d639182d6020d251762174
Olieveira <nathanmello2@outlook.com>
2022-07-16 23:17:43 -0300

    Img errors fixed

--- components/Menu.js
+++ components/Menu.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, Carousel, NavbarBrand, NavLink, NavbarToggler, Collapse, Nav, NavItem, Accordion, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText, Container } from 'reactstrap';
-import Image from 'react'
-import Logo from '../src/logo.svg'
+import { Navbar, Jumbotron ,Carousel, NavbarBrand, NavLink, NavbarToggler, Collapse, Nav, NavItem, Accordion, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText, Container } from 'reactstrap';
+
+import Image from 'next/image';
+import logo from '.././src/logo.png';
+import Img from 'react-image';
 
 const Menu = () => {
 
@@ -15,8 +17,14 @@ const Menu = () => {
             <Navbar color="dark" dark expand="md" style={{ backgroundImage: `linear-gradient(to right, #1d1a1a, #957653)`, }}>
                 <Container>
 
-                    <img src={Logo} />
-                    <NavbarBrand href='/'>Studio Caroline Vitória</NavbarBrand>
+                    <Image
+                        src={logo}
+                        height={"100px"}
+                        width={"250px"}
+                        alt={'Studio Caroline Vitória'}
+                        layout={'responsive'}
+                        placeholder={'blur'}
+                    />
 
                     <NavbarToggler onClick={toggle} />
 
--- pages/index.js
+++ pages/index.js
@@ -3,18 +3,24 @@ import Menu from '../components/Menu';
 import Head from 'next/head';
 import { styled } from '../src/stitches.config';
 import Icone from '../public/favicon.ico'
+import { Nav, Container } from 'reactstrap'
 
 
 function App() {
     return (
         <div>
             <Head>
-                <meta name='description' content='Studio de estética e beleza, Micropigmentação, Henna, fio a fio, Estudio de estética'/>
-                <meta name='author' content='Studio CV'/>
+                <meta name='description' content='Studio de estética e beleza, Micropigmentação, Henna, fio a fio, Estudio de estética' />
+                <meta name='author' content='Studio CV' />
                 <link rel='shortcut icon' href={Icone} type='image/x-icon' />
                 <title>Studio CV</title>
             </Head>
             <Menu />
+            <div>
+                <Container>
+                    <h1>Just testing</h1>
+                </Container>
+            </div>
         </div>
     )
 };
